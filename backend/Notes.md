b9e0646fe2ffd79dac0009fd91c058976d3da201
sujay <jirapuresujay@gmail.com>
2024-03-21 23:23:50 +0530

    new

--- backend/Notes.md
+++
@@ -1,6 +0,0 @@
-
-Backend 
-
-<h6>step 1</h6> 
-
-need to install 
--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,20 @@ app.get('/',(req ,resp) => {
     //console.log('sever started');   
 })
 
+app.get('/home',(req ,resp) => {
+    const productlist = [
+        {
+            name: "product 1",
+            id: 1
+        },
+        {
+            name: "product 2",
+            id: 2
+        }
+    ]
+    resp.send(productlist);
+    console.log('product api hit...'); 
+})
 
 console.log("sujay server is running...");
-app.listen(5000);
+app.listen(4000);
