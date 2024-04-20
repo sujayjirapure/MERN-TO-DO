d4b9b67a79f78b6f5bcdba71d04e8bcffd324ef6
sujay <jirapuresujay@gmail.com>
2024-04-20 16:04:36 +0530

    all files

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json());  //imp for post
 
 app.get('/',(req ,resp) => {
     resp.send('my server is started'); 
@@ -12,15 +13,22 @@ app.get('/home',(req ,resp) => {
         {
             name: "product 1",
             id: 1
-        },
-        {
-            name: "product 2",
-            id: 2
         }
     ]
     resp.send(productlist);
     console.log('product api hit...'); 
 })
+
+app.post('/', (req, resp) => {
+    try{
+        console.log('post api data -', req.body);
+        resp.send('api sucessfull...');
+    }
+    catch(err){
+        resp.send("faild...");
+    }
+})
+
 
 console.log("sujay server is running...");
 app.listen(4000);
