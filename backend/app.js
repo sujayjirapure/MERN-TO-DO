ee967563d71330e16eeb15438d995bf2e4dff51b
sujay <jirapuresujay@gmail.com>
2024-05-13 15:53:43 +0530

    dfedf

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,50 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); 
+const cors = require('cors');
+app.use(cors());
 app.use(bodyParser.json());  //imp for post
+const mongoose = require('mongoose');
 
+
+//scehma 
+const userSchema = new mongoose.Schema({
+    work: String,
+    deadline: String
+})
+
+const todomern = mongoose.model('todomern', userSchema)
+//    anything                 table name       same as upper schema (to coonnect)
+
+//default 
 app.get('/',(req ,resp) => {
     resp.send('my server is started'); 
     //console.log('sever started');   
 })
 
-app.get('/home',(req ,resp) => {
-    const productlist = [
-        {
-            name: "product 1",
-            id: 1
-        }
-    ]
-    resp.send(productlist);
-    console.log('product api hit...'); 
+//show all data from data base to front end
+app.get('/data',async (req ,resp) => {
+   const docs = await todomern.find({});
+   resp.json(docs);   
 })
 
-app.post('/', (req, resp) => {
+//deletee
+app.delete("/deletetodo/:id", async (req ,resp) => {
+    const id = req.params.id
+    //console.log(id);     printing on back console
+    const data = await todomern.deleteOne({_id : id})
+    resp.send("deleted...");
+})
+
+app.post('/',async (req, resp) => {
     try{
-        console.log('post api data -', req.body);
-        resp.send('api sucessfull...');
+        let user = new todomern();
+        user.work = req.body.work;
+        user.deadline = req.body.deadline;
+        const doc = await user.save();
+        
+        console.log('post api data -', doc);
+        
     }
     catch(err){
         resp.send("faild...");
@@ -30,5 +52,16 @@ app.post('/', (req, resp) => {
 })
 
 
-console.log("sujay server is running...");
+//connect DB
+// const dburl = 'mongodb+srv://sujay03:sujay03@cluster0.23hwmxr.mongodb.net/restapi?retryWrites=true&w=majority';
+
+const dburl = 'mongodb+srv://sujay03:sujay03@cluster0.23hwmxr.mongodb.net/todomern?retryWrites=true&w=majority';
+
+mongoose.connect(dburl).then(() => {
+    console.log('mongooDB is connected...');
+}).catch((err) => console.log('MongoDB not connected...'));
+ 
+
+
+console.log("sujay server is running at 4000...");
 app.listen(4000);
---
+++ frontend/src/App.js
@@ -0,0 +1,111 @@
+import React, { useState ,useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+
+
+
+function App() {
+  const [from ,setFrom] = useState({});
+  const [showdata ,setShowdata] = useState([]);
+
+  const handleform = (e)=>{
+    setFrom({
+      ...from,
+      [e.target.name] : e.target.value       //[e.target.name] - will be universal and take automatic
+    });
+  }
+
+  const handlesubmit = async (e)=>{     //post request
+    //e.preventDefault();
+    const response = await fetch('http://localhost:4000/',{
+    method:'POST',
+    body:JSON.stringify(from),
+    headers:{
+      'Content-Type':'application/json'
+    }
+
+    })
+    const data = await response.json();
+  }
+
+  //get all data from DB
+  const getdata = async () => {
+    const response = await fetch('http://localhost:4000/data',{
+    method:'GET',
+    })
+    const dataget = await response.json();
+    setShowdata(dataget);
+  }
+
+  useEffect(()=>{
+    getdata();
+    //console.log("ready to display the DB data",showdata);   //shown in console
+  },[])
+
+  //delete todo task
+  const tododelete = async (id) => {
+
+    await fetch('http://localhost:4000/deletetodo/'+id,{
+    method:'DELETE',
+    })
+    window.location.reload();
+  }
+
+  return (
+        <div className='App-header'>
+          <h1>TO-DO LIST </h1>
+          <Form onSubmit={handlesubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Enter task</Form.Label>
+              <Form.Control type='text' name='work' placeholder="TASK" onChange={handleform} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Enter Deadline</Form.Label>
+              <Form.Control type='text' name='deadline' onChange={handleform} placeholder="Deadline" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+            <br></br>
+             {/* <div>
+              <ul>
+              {
+                showdata.map(user => <li key={user._id}>{user.work} ,{user.deadline}<button onClick={()=>tododelete(user._id)}>delete</button></li>)
+              }
+              </ul>
+            </div>  */}
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>Sr </th>
+                  <th>TASK</th>
+                  <th>Deadline</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  showdata.map((user ,index )=>{
+                    return(
+                      <tr>
+                        <td>{index + 1}</td>
+                        <td>{user.work}</td>
+                        <td>{user.deadline}</td>
+                        <td><button className='dlt-btn' onClick={()=>tododelete(user._id)}>Delete</button></td>
+                    </tr>
+                    )
+                  })
+                
+                }
+              </tbody>
+            </Table>
+            
+      </div>
+  );
+}
+
+export default App;
